93214148c583b178d4e27279f39599061d7ddc18
satoru314 <satoru314@MSI.>
2025-10-14 14:29:27 +0900

    initial commit

---
+++ api/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"github.com/yourname/reponame/api"
+)
+
+func main() {
+	// .envファイルを読み込む
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbDatabase := os.Getenv("DB_NAME")
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		log.Println("fail to connect DB")
+		return
+	}
+
+	r := api.NewRouter(db)
+
+	log.Println("server start at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
